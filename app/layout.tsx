738d53e7c25d11ef98e227f79987bbd660a4e296
Sangwoo Park <sangwopark19@gmail.com>
2024-08-07 16:10:06 +0900

    feat: 회원가입, 로그인을 위한 Modal 컴포넌트 생성

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import { Inter } from "next/font/google";
 
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,7 +28,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Modal actionLabel="계속" title="Hello World" isOpen/>
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
